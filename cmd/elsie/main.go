e1bfe882b0a33d481c716865201bf3b8cf628e20
Scott Moynes <smoynes@users.noreply.github.com>
2023-09-08 19:50:51 -0400

    There has to be a better way!

--- cmd/elsie/main.go
+++ cmd/elsie/main.go
@@ -7,7 +7,9 @@ import (
 
 func main() {
 	machine := cpu.New()
-	machine.Mem.Store(cpu.Word(machine.PC), cpu.Word(cpu.OpcodeAND)<<12|0x0040|0x0011)
+	machine.Mem.MAR = cpu.Register(machine.PC)
+	machine.Mem.MDR = cpu.Register(cpu.OpcodeAND)<<12 | 0x0040 | 0x0011
+	machine.Mem.Store()
 	machine.Reg[cpu.R0] = 0xffff
 	print(machine.String(), "\n")
 	print(machine.Reg.String(), "\n")
