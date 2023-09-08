44ae23f7c0e45e2a4bbbed8cd194a823b787e29e
Scott Moynes <smoynes@users.noreply.github.com>
2023-09-08 16:22:50 -0400

    Add tool dependencies

--- internal/cpu/strings.go
+++ internal/cpu/strings.go
@@ -1,3 +1,3 @@
 package cpu
 
-//go:generate stringer -type=Opcode,GPR,Privilege,Priority,offset,literal,vector -output=strings_gen.go -linecomment
+//go:generate go run golang.org/x/tools/cmd/stringer -type=Opcode,GPR,Privilege,Priority,offset,literal,vector -output=strings_gen.go -linecomment
---
+++ tools.go
@@ -0,0 +1,9 @@
+//go:build tools
+// +build tools
+
+// Package tools declares Go tool dependencies.
+package tools
+
+import (
+	_ "golang.org/x/tools/cmd/stringer"
+)
