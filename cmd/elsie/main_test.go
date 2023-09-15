7cab1c746f5f387ca6432cabc56fde0f313b0a46
Scott Moynes <smoynes@users.noreply.github.com>
2023-09-15 19:40:27 -0400

    Finally some concurrency

--- cmd/elsie/main_test.go
+++ cmd/elsie/main_test.go
@@ -15,23 +15,15 @@ func init() {
 	log.Default().SetOutput(io.Discard)
 }
 
+const timeout = time.Second
+
 func TestMain(t *testing.T) {
 	ctx := context.Background()
 	mach := vm.New(
 		vm.WithLogger(log.Default()),
 	)
 
-	t.Log(mach.String())
-	t.Log(mach.Reg.String())
-	t.Log(mach.PC.String())
-	t.Log(mach.IR.String())
-	t.Log(mach.MCR.String())
-	t.Log(mach.Mem.MAR.String())
-	t.Log(mach.Mem.MDR.String())
-	t.Log(mach.SSP.String())
-	t.Log(mach.USP.String())
-
-	ctx, cancel := context.WithTimeoutCause(ctx, 5*time.Second, errors.New("bad"))
+	ctx, cancel := context.WithTimeoutCause(ctx, timeout, errors.New("bad"))
 	defer cancel()
 
 	go func() {
@@ -47,13 +39,38 @@ func TestMain(t *testing.T) {
 		cancel()
 	}()
 
-	select {
-	case <-ctx.Done():
-		err := ctx.Err()
-		if !errors.Is(err, context.DeadlineExceeded) {
-			t.Error(err)
-		} else if errors.Is(err, context.Canceled) {
-			return
+	start := time.Now()
+
+loop:
+	for {
+		t.Log(mach.String())
+		t.Log(mach.Reg.String())
+		t.Log(mach.PC.String())
+		t.Log(mach.IR.String())
+		t.Log(mach.MCR.String())
+		t.Log(mach.Mem.MAR.String())
+		t.Log(mach.Mem.MDR.String())
+		t.Log(mach.SSP.String())
+		t.Log(mach.USP.String())
+		t.Log("")
+		select {
+		case <-time.After(100 * time.Millisecond):
+		case <-ctx.Done():
+			break loop
 		}
 	}
+
+	cause, err := context.Cause(ctx), ctx.Err()
+	switch {
+	case errors.Is(cause, context.Canceled):
+		// ok
+	case errors.Is(err, context.DeadlineExceeded):
+		// too slow
+		t.Errorf("run took more than %v", timeout)
+	default:
+		t.Errorf("unexpected error: %s", err)
+	}
+
+	t.Logf("took %s", time.Since(start))
+	t.Fail()
 }
