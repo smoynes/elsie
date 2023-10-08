ce10cd3483f7c4b2193427f92f6df29f902a86a5
Scott Moynes <smoynes@users.noreply.github.com>
2023-10-08 16:45:09 -0400

    Use "standard" labels

--- internal/asm/testdata/parser6.asm
+++ internal/asm/testdata/parser6.asm
@@ -5,8 +5,7 @@
     TRAP    x23                 ; Read char input:    R0 <- CHAR
     LDR     R1,R3,#0            ; Fetch next char:    R1 <- [PTR]
 
-TEST:
-    ADD     R4,R1,#-4           ; Test for EOT.
+TEST ADD     R4,R1,#-4           ; Test for EOT.
     BRz     OUTPUT              ; If true, exit loop: TEST.
 
     ;; Test for match.
@@ -16,14 +15,12 @@ TEST:
     BRnp    GETCHAR             ; If not match then do not increment.
     ADD     R2,R2,#1            ; Increment counter:  R2 <- R2+1
 
-GETCHAR:
-    ADD     R3,R3,#1            ; Increment pointer: R3 <- R3+1
+GETCHAR ADD     R3,R3,#1            ; Increment pointer: R3 <- R3+1
     LDR     R1,R3,#0            ; Fetch next char:   R1 <- [PTR]
     BRnzp   TEST                ; Loop:              TEST
 
     ;; Display output.
-OUTPUT:
-    LD      R0,ASCII            ; Load ASCII text base:     R0 <- x0030
+OUTPUT LD      R0,ASCII            ; Load ASCII text base:     R0 <- x0030
     ADD     R0,R0,R2            ; Add offset to ASCII base: R0 <- R2+0x0030
     TRAP    x21                 ; Print ASCII code:         IO <- R0
     TRAP    x25                 ; Halt and catch fire.
