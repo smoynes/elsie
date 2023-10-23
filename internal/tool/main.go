841762c498172d659f421b3c98da4298ca96aa66
Scott Moynes <smoynes@users.noreply.github.com>
2023-10-23 13:19:39 -0400

    Add some developer niceties
    Adds:
    
    - a Dockerfile for building a (portable?) binary
    - a development guide
    - move docs for cleanliness
    - a tool for automating development tasks
    
    On the later, some explanation is included in the docs but more, I suspect, is
    warranted in history as well. The idea is to avoid adding a dependency on make
    or /bin/sh or any other (portable?) tool for command scripting. Although, it is
    verbose and a bit weird, it is eminently possible to run commands, parse output,
    handle errors, and solve all the problems a scripting language introduces
    without adding another necessary tool to the developer's workbench.
    
    For our purposes, it might even be better (simpler, easier to learn, more
    stable) to write a bare-essential scripting-language to solve the problem. We
    are programmers and when faced with programming problems we ought not be afraid
    to solve them with _our own_ programs.
    
    Squashed commit of the following: commit 7701250e..95652d16

---
+++ internal/tool/main.go
@@ -0,0 +1,153 @@
+// Package tool defines very naive scripts for development tasks. These are not intended to be
+// portable and simply replace equivalent shell scripts. Just like shell, it is a miracle when these
+// scripts work at all.
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	path "path/filepath"
+)
+
+var usage = `go run internal/tool <COMMAND>
+
+Commands:
+
+- deps       checks build dependencies: (stringer, docker)
+- container  builds docker image: smoynes/elsie
+- lint       check style with golangci-lint
+`
+
+func main() {
+	args := os.Args
+	dir, err := os.Getwd()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		file := path.Join(dir, "go.mod")
+
+		if _, err := os.Stat(file); err == nil {
+			break
+		} else if os.IsNotExist(err) {
+			dir = path.Dir(dir)
+		} else {
+			log.Fatal(err)
+		}
+	}
+
+	if err := os.Chdir(dir); err != nil {
+		log.Fatal(err)
+	}
+
+	switch {
+	case len(args) == 2 && os.Args[1] == "deps":
+		if err := deps(); err != nil {
+			log.Fatal(err)
+		}
+	case len(args) == 2 && os.Args[1] == "container":
+		if err := dockerBuild(); err != nil {
+			log.Fatal(err)
+		}
+	case len(args) == 2 && os.Args[1] == "lint":
+		if err := golangciLint(); err != nil {
+			log.Fatal(err)
+		}
+	default:
+		fmt.Fprintf(os.Stderr, "Usage: %s\n", usage)
+	}
+}
+
+func deps() error {
+	if stringer, err := exec.LookPath("stringer"); err != nil {
+		return fmt.Errorf("stringer (required): %w", err)
+	} else {
+		fmt.Println("stringer (required):", stringer)
+	}
+
+	if linter, err := exec.LookPath("golangci-lint"); err != nil {
+		return fmt.Errorf("golangci-lint (optional): %w", err)
+	} else {
+		fmt.Println("golangci-lint (optional):", linter)
+	}
+
+	if docker, err := exec.LookPath("docker"); err != nil {
+		return fmt.Errorf("docker (optional): version: %w", err)
+	} else {
+		fmt.Println("docker (optional):", docker)
+	}
+
+	return nil
+}
+
+func dockerBuild() error {
+	docker := exec.Command("docker", "build", "-t", "smoynes/elsie", ".")
+	out, err := docker.StderrPipe()
+
+	if err != nil {
+		return fmt.Errorf("docker: pipe: %w", err)
+	}
+
+	if err = docker.Start(); err != nil {
+		return fmt.Errorf("docker: build: %w", err)
+	}
+
+	fmt.Println("docker build:")
+
+	for {
+		copied, err := io.Copy(os.Stdout, out)
+		if err != nil {
+			return fmt.Errorf("docker: io: %w", err)
+		}
+
+		if copied == 0 {
+			break
+		}
+	}
+
+	if err = docker.Wait(); err != nil {
+		return fmt.Errorf("docker: wait: %w", err)
+	}
+
+	println("\n\nBuilt container:")
+	println("\tdocker run smoynes/elsie")
+
+	return nil
+}
+
+func golangciLint() error {
+	linter := exec.Command("golangci-lint", "run")
+	out, err := linter.StdoutPipe()
+
+	if err != nil {
+		return fmt.Errorf("golangci-lint: pipe: %w", err)
+	}
+
+	if err = linter.Start(); err != nil {
+		return fmt.Errorf("golangci-lint: run: %w", err)
+	}
+
+	fmt.Println("golangci-lint:")
+
+	for {
+		copied, err := io.Copy(os.Stdout, out)
+		if err != nil {
+			return fmt.Errorf("golangci-lint: io: %w", err)
+		}
+
+		if copied == 0 {
+			break
+		}
+	}
+
+	if err = linter.Wait(); err != nil {
+		return fmt.Errorf("golangci-lint: wait: %w", err)
+	}
+
+	return nil
+}
