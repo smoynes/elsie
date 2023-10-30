d46fe09378cf846978045022801cb2a133d2854e
Scott Moynes <smoynes@users.noreply.github.com>
2023-10-30 09:05:17 -0400

    Add trap OUT test case

--- internal/monitor/traps_test.go
+++ internal/monitor/traps_test.go
@@ -19,6 +19,10 @@ func (*trapHarness) Logger() *log.Logger {
 func TestTrap_Halt(tt *testing.T) {
 	t := trapHarness{tt}
 
+	if testing.Verbose() {
+		log.LogLevel.Set(log.Debug)
+	}
+
 	obj, err := Generate(TrapHalt)
 
 	if err != nil {
@@ -79,6 +83,82 @@ func TestTrap_Halt(tt *testing.T) {
 	}
 
 	if machine.MCR.Running() {
+		t.Error("pc", machine)
+	}
+}
+
+func TestTrap_Out(tt *testing.T) {
+	t := trapHarness{tt}
+
+	if testing.Verbose() {
+		log.LogLevel.Set(log.Debug)
+	}
+
+	obj, err := Generate(TrapOut)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if len(obj.Code) < 9 {
+		// Code must be AT LEAST 15 words: 12 instructions and a few bytes of data.
+		t.Error("code too short", len(obj.Code))
+	} else if len(obj.Code) >= 50 {
+		t.Error("code too long", len(obj.Code))
+	}
+
+	// We want to test the trap in isolation, without any other traps loaded.
+	image := SystemImage{
+		log:     t.Logger(),
+		Symbols: nil,
+		Traps: []Routine{
+			TrapOut,
+		},
+	}
+
+	machine := vm.New(
+		WithSystemImage(&image),
+	)
+	loader := vm.NewLoader(machine)
+
+	machine.REG[vm.R0] = 0x3010
+	msg := vm.ObjectCode{
+		Orig: 0x3010,
+		Code: []vm.Word{
+			0x003A,
+			0x003B,
+			0x003C,
+			0x0021,
+			0x0000,
+		},
+	}
+
+	loader.Load(msg)
+
+	code := vm.ObjectCode{
+		Orig: 0x3000,
+		Code: []vm.Word{
+			vm.Word(vm.NewInstruction(
+				vm.TRAP,
+				uint16(vm.TrapOUT)).Encode(),
+			),
+		},
+	}
+	loader.Load(code)
+
+	for i := 0; i < 200; i++ {
+		if machine.IR < 0x3000 {
+			continue
+		}
+
+		t.Logf("DDR")
+		err = machine.Step()
+		if err != nil {
+			t.Error(err)
+		}
+	}
+
+	if !machine.MCR.Running() {
 		t.Error("pc", machine)
 	}
 }
