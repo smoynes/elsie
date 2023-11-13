3253bc37a65cb9c069cb99ecda0aec905bb1d965
Scott Moynes <smoynes@users.noreply.github.com>
2023-11-12 23:37:22 -0500

    Expand hex decoding

--- internal/encoding/hex.go
+++ internal/encoding/hex.go
@@ -3,15 +3,119 @@
 package encoding // import github.com/smoynes/elsie/internal/encoding
 
 import (
+	"bufio"
+	"bytes"
+	"encoding/binary"
+	"encoding/hex"
 	"errors"
-)
-w
-type HexEncoder struct{}
+	"fmt"
+	"io"
 
-func (h *HexEncoder) MarshalText() ([]byte, error) {
+	"github.com/smoynes/elsie/internal/vm"
+)
+
+// HexEncoding implements marshalling and unmarshalling of Intel Hex files.
+type HexEncoding struct {
+	code *vm.ObjectCode
+}
+
+func (h HexEncoding) Code() vm.ObjectCode {
+	code := vm.ObjectCode{}
+	if h.code != nil {
+		code = *h.code // copy
+	}
+
+	return code
+}
+
+func (h *HexEncoding) MarshalText() ([]byte, error) {
 	return nil, errors.New("not implemented")
 }
 
-func (h *HexEncoder) UnmarshalText(bs []byte) error {
-	return errors.New("not implemented")
+func (h *HexEncoding) UnmarshalText(bs []byte) error {
+	buf := bufio.NewReader(bytes.NewReader(bs))
+	code := []vm.ObjectCode(nil)
+
+	for { // iterate over lines in buffer
+
+		var (
+			recLen  int8   // Number of bytes in data field; excludes address, type, checksum fields.
+			recAddr uint16 // Record address.
+		)
+
+		// First, look for leading ':'
+		if prefix, err := buf.ReadByte(); err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		} else if prefix == '\n' {
+			continue
+		} else if prefix != ':' {
+			return fmt.Errorf("%w: line does not start with ':'",
+				invalidEncodingErr)
+		}
+
+		// Next, read two hexadecimal bytes.
+		var countBytes [2]byte
+		if _, err := io.ReadFull(buf, countBytes[:]); err != nil {
+			return err
+		}
+
+		// Decode hex bytes into a byte value.
+		if _, err := hex.Decode(countBytes[:1], countBytes[:]); err != nil {
+			return fmt.Errorf("%w: %s", invalidEncodingErr, err)
+		}
+
+		// Convert the byte into an integer containing the record length.
+		err := binary.Read(bytes.NewReader(countBytes[:1]), binary.BigEndian, &recLen)
+		if err != nil {
+			return fmt.Errorf("%w: %s", invalidEncodingErr, err)
+		}
+
+		// Eat to end of line. (?)
+		if _, err := buf.ReadBytes('\n'); err == io.EOF {
+			break
+		}
+	}
+
+	if len(code) == 0 {
+		return emptyErr
+	}
+
+	return nil
 }
+
+// kind represents the type of encoded record. Only the subset of record types supported by the
+// encoder are supported.
+type kind byte
+
+const (
+	kindData        kind = 0
+	kindEOF         kind = 1
+	kindSegmentAddr kind = 2
+	kindLinearAddr  kind = 4
+)
+
+type decodingError struct{}
+
+func (decodingError) Error() string {
+	return "decoding error"
+}
+
+func (de *decodingError) Is(err error) bool {
+	if de == err {
+		return true
+	} else if _, ok := err.(*decodingError); ok {
+		return true
+	} else {
+		return false
+	}
+}
+
+var (
+	// DecodingErr is a wrapped error that is returned when decoding fails.
+	DecodingErr = &decodingError{}
+
+	emptyErr           = fmt.Errorf("%w: no data decoded", DecodingErr)
+	invalidEncodingErr = fmt.Errorf("%w: invalid encoding", DecodingErr)
+)
--- internal/encoding/hex_test.go
+++ internal/encoding/hex_test.go
@@ -1,18 +1,91 @@
-package encoding_test
+package encoding
 
 import (
 	"encoding"
+	"errors"
 	"testing"
 
-	. "github.com/smoynes/elsie/internal/encoding"
+	"github.com/smoynes/elsie/internal/vm"
 )
 
 // Assert interface implemented.
 var (
-	_ encoding.TextMarshaler   = (*HexEncoder)(nil)
-	_ encoding.TextUnmarshaler = (*HexEncoder)(nil)
+	_ encoding.TextMarshaler   = (*HexEncoding)(nil)
+	_ encoding.TextUnmarshaler = (*HexEncoding)(nil)
 )
 
-func TestHexEncoder_MarshalText(t *testing.T) {
+type unmarshalTestCase struct {
+	name, input, inputFilename string
 
+	expectCode vm.ObjectCode
+	expectErr  error
+}
+
+func TestHexEncoder_UnmarshalText(t *testing.T) {
+	tcs := []unmarshalTestCase{
+		{
+			name:          "empty",
+			input:         "",
+			inputFilename: "",
+			expectErr:     emptyErr,
+		},
+		{
+			name:      "eof record",
+			input:     ":000001FF",
+			expectErr: emptyErr,
+		},
+		{
+			name:      "eof record with newlines",
+			input:     "\n\n:000001FF\n\n",
+			expectErr: emptyErr,
+		},
+		{
+			name:      "invalid bytes",
+			input:     ":invalid",
+			expectErr: invalidEncodingErr,
+		},
+		{
+			name:      "nonsense",
+			input:     "u wot mate",
+			expectErr: invalidEncodingErr,
+		},
+		{
+			name:      "data record",
+			input:     ":000102cafe",
+			expectErr: nil,
+		},
+	}
+
+	for _, tc := range tcs {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			code, err := unmarshal(tc)
+
+			t.Logf("got: %v, err: %v", code, err)
+
+			switch {
+			case tc.expectErr != nil && err != nil:
+				if !errors.Is(err, tc.expectErr) {
+					t.Errorf("Unexpected error: got: %s, want: %s",
+						err.Error(), tc.expectErr.Error())
+				}
+			case tc.expectErr != nil && err == nil:
+				t.Errorf("Expected error: %s", tc.expectErr.Error())
+			case tc.expectErr == nil && err != nil:
+				t.Errorf("Unexpected error: got: %v", err)
+			default:
+				if code.Orig != tc.expectCode.Orig {
+					t.FailNow()
+				}
+			}
+
+		})
+	}
+}
+
+func unmarshal(tc unmarshalTestCase) (vm.ObjectCode, error) {
+	decoder := HexEncoding{}
+	err := decoder.UnmarshalText([]byte(tc.input))
+	return decoder.Code(), err
 }
